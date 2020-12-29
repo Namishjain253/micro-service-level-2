4eda0b467140a6c104f9a725ea98a13d57869242
unknown <n2n.infotech@gmail.com>
2020-12-29 18:24:09 +0530

    Latest Update with Full Example for Level 2

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/services/MovieInfo.java
@@ -0,0 +1,36 @@
+package io.javabrains.moviecatalogservice.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+
+import io.javabrains.moviecatalogservice.models.CatalogItem;
+import io.javabrains.moviecatalogservice.models.Movie;
+import io.javabrains.moviecatalogservice.models.Rating;
+
+@Service
+public class MovieInfo {
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+//	@HystrixCommand(fallbackMethod = "getFallbackCatelogItem")
+	@HystrixCommand(fallbackMethod = "getFallbackCatelogItem", 
+			threadPoolKey = "movieInfoPool",
+			threadPoolProperties = {
+					@HystrixProperty(name = "coreSize", value = "20"),
+					@HystrixProperty(name = "maxQueueSize", value = "10")
+			})
+	public CatalogItem getCatelogItem(Rating rating) {
+		Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+		return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+	}
+
+	public CatalogItem getFallbackCatelogItem(Rating rating) {
+		return new CatalogItem("Movie Name Not Found", "", rating.getRating());
+	}
+	
+}
